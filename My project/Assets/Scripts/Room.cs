9c40539ec46bef6277bbc04daacea0cb9fb9ae57
GM07 <mehgaya@gmail.com>
2022-05-14 05:17:28 -0400

    map generation objects

--- My project/Assets/Scripts/Room.cs
+++ My project/Assets/Scripts/Room.cs
@@ -14,24 +14,47 @@ public class Room : MonoBehaviour
 
     public GameObject[] walls;
     public GameObject[] doors;
+    public GameObject baril;
 
+    public Color roomColor;
 
     // Start is called before the first frame update
     void Start()
     {
-    }
-
-    public void UpdateRooms(bool[] status)
-    {
-        for (int i = 0; i < status.Length; i++) {
-            // doors[i].SetActive(status[i]);
-            walls[i].SetActive(!status[i]);
-        }
+        CreateObjects();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CreateObjects()
+    {
+        int nbObject = Random.Range(0, 5);
+
+        int counter = 0;
+        Debug.Log("Center : " + transform.position);
+        while (counter++ < nbObject)
+        {
+            float angle = Random.Range(0, 360);
+            float dx = 25.0f * 0.75f * Mathf.Cos(angle);  
+            float dz = 25.0f * 0.75f * Mathf.Sin(angle);
+
+            Vector3 position = transform.position + new Vector3(dx, 0, dz);
+            GameObject obj = Instantiate(baril, position, Quaternion.Euler(new Vector3(90, 90, 0)), transform);
+            var objTransform = obj.GetComponent<Transform>();
+            objTransform.position = objTransform.position + new Vector3(0, objTransform.localScale.y * 1.25f, 0);
+        }
+    }
+
+    public void UpdateRooms(bool[] status)
+    {
+        for (int i = 0; i < status.Length; i++)
+        {
+            // doors[i].SetActive(status[i]);
+            walls[i].SetActive(!status[i]);
+        }
     }
 }
