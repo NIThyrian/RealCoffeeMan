ff25d5902368932ca4ff3417bbd3ccf09cdf7040
Mathieu Brodeur <matbrodeur1@gmail.com>
2022-05-14 18:31:01 -0400

    finished shop

--- My project/Assets/Scripts/Game.cs
+++ My project/Assets/Scripts/Game.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,30 +6,32 @@ public class Game : MonoBehaviour
     public Color currentColor;
     public int level;
     public GameObject map;
-
     private GameObject mapReference; 
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public Dictionary<string, int> playerDict = new Dictionary<string, int> {
+            {"SteakPurchased", 10},
+            {"GunPurchased", 1},
+            {"BootsPurchased", 1},
+            {"CoffeePurchased", 1},
+            {"CaHeld", 1},
+            {"GoldHeld", 1},
+            {"NotACubeHeld", 1},
+            {"PoopHeld", 1},
+            {"RocketHeld", 1},
+            {"CashHeld", 100}
+        };
+
+    void Start() {
         level = 1;
         currentColor = Random.ColorHSV();
         mapReference = Instantiate(map, new Vector3(0.0f,0.0f,0.0f), Quaternion.identity, transform);
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("f"))
-        {
-            ChangeLevel();
-        }
-
+    void Update() {
+        if (Input.GetKeyDown("f")) ChangeLevel();
     }
 
-    void ChangeLevel()
-    {
+    void ChangeLevel() {
         Destroy(mapReference);
         currentColor = Random.ColorHSV();
         level++;
